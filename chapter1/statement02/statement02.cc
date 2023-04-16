1bc075372276263d5691896c3af22df6dfa0075a
wpaladins <wpaladins@163.com>
2023-04-17 01:31:12 +0800

    chapter1 - statement02

---
+++ chapter1/statement02/statement02.cc
@@ -0,0 +1,96 @@
+#include "statement02.h"
+
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+#include <iostream>
+#include <sstream>
+
+using namespace std;
+
+string statement02(const invoice_t& invoce, const plays_t& plays) {
+    // 以查询取代临时变量(178)
+    auto playFor = [&plays](const performance_t& aPerformance) -> const play_t& { // 注意返回值类型
+        return plays.at(aPerformance.playID); // 使用 at(), 因为 plays 是 const 的
+    };
+
+    // 提炼函数(106)
+    // 该表函数声明(124)
+    auto amountFor = [&](const performance_t& aPerformance) {
+        int result = 0;
+
+        switch (playFor(aPerformance).type)
+        {
+        case TRAGEDY_TYPE:
+            result = 40000;
+            if (aPerformance.audience > 30) {
+                result += 1000 * (aPerformance.audience - 30);
+            }
+            break;
+
+        case COMEDY_TYPE:
+            result = 30000;
+            if (aPerformance.audience > 20) {
+                result += 10000 + 500 * (aPerformance.audience - 20);
+            }
+            result += 300 * aPerformance.audience;
+            break;
+
+        default:
+            exit(-1);
+        }
+        return result;
+    };
+
+    auto volumeCreditsFor = [&](const performance_t& aPerformance) {
+        int result = 0;
+        result += max(aPerformance.audience - 30, 0);
+        // 内联变量(123)
+        if (COMEDY_TYPE == playFor(aPerformance).type) {
+            result += floor(aPerformance.audience / 5);
+        }
+        return result;
+    };
+
+    // 改变函数声明(124)
+    // https://stackoverflow.com/questions/14520309/the-precision-of-stdto-stringdouble
+    auto usd = [](int aNumber) {
+        string number_str = to_string(float(aNumber)/100);
+        return "$" + number_str.substr(0, number_str.find(".") + 3);
+    };
+
+    // 以查询取代临时变量(178)
+    // 提炼函数(106)
+    auto totalVolumeCredits = [&]() {
+        // 移动语句(223)
+        int result = 0;
+        for (auto& perf : invoce.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    };
+
+    auto totalAmount = [&]() {
+        int result = 0;
+        for (auto& perf : invoce.performances) {
+            result += amountFor(perf);
+        }
+        return result;
+    };
+
+    ostringstream result;
+    result << "Statement for " << invoce.customer << endl;
+
+    // 拆分循环(227)
+    for (auto& perf : invoce.performances) {
+        // 内联变量(123)
+        result << "  " << playFor(perf).name + ": "
+            << usd(amountFor(perf)) << " ("
+            << to_string(perf.audience) << " seats)" << endl;
+    }
+
+    result << "Amount owed is " << usd(totalAmount()) << endl;
+    // 内联变量(123)
+    result << "You earned " << to_string(totalVolumeCredits()) << " credits" << endl;
+    return move(result).str();
+}
---
+++ chapter1/statement02/statement02.h
@@ -0,0 +1,9 @@
+#ifndef REFACTORING_2ND_CHAPTER1_STATEMENT02_STATEMENT02_H
+#define REFACTORING_2ND_CHAPTER1_STATEMENT02_STATEMENT02_H
+
+#include "plays.h"
+#include "invoices.h"
+
+std::string statement02(const invoice_t& invoce, const plays_t& plays);
+
+#endif  // REFACTORING_2ND_CHAPTER1_STATEMENT02_STATEMENT02_H
