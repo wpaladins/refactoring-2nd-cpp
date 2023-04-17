c3dd1edd2cbcf6cc49d8bc15a124c4711616c88d
wpaladins <wpaladins@163.com>
2023-04-17 20:27:12 +0800

    chapter1 - statement03 - 5

--- chapter1/statement03/statement03.cc
+++ chapter1/statement03/statement03.cc
@@ -11,6 +11,7 @@ using namespace std;
 // 复制 performance_t 中的信息, 而其他信息仅引用
 struct enriched_performance_t : public performance_t {
     const play_t& play;
+    int amount;
     enriched_performance_t(const performance_t& parent,
                            const play_t& play)
                            : performance_t(parent),
@@ -26,6 +27,58 @@ struct statement_data_t {
 
 // 保持数据不可变(不修改传给函数的参数)
 string renderPlainText(const statement_data_t& data, const plays_t& plays) {
+    auto volumeCreditsFor = [&](const enriched_performance_t& aPerformance) {
+        int result = 0;
+        result += max(aPerformance.audience - 30, 0);
+        if (COMEDY_TYPE == aPerformance.play.type) {
+            result += floor(aPerformance.audience / 5);
+        }
+        return result;
+    };
+
+    auto usd = [](int aNumber) {
+        string number_str = to_string(float(aNumber)/100);
+        return "$" + number_str.substr(0, number_str.find(".") + 3);
+    };
+
+    auto totalVolumeCredits = [&]() {
+        int result = 0;
+        for (auto& perf : data.performances) {
+            result += volumeCreditsFor(perf);
+        }
+        return result;
+    };
+
+    auto totalAmount = [&]() {
+        int result = 0;
+        for (auto& perf : data.performances) {
+            result += perf.amount;
+        }
+        return result;
+    };
+
+    ostringstream result;
+    result << "Statement for " << data.customer << endl;
+
+    for (auto& perf : data.performances) {
+        result << "  " << perf.play.name + ": "
+            << usd(perf.amount) << " ("
+            << to_string(perf.audience) << " seats)" << endl;
+    }
+
+    result << "Amount owed is " << usd(totalAmount()) << endl;
+    result << "You earned " << to_string(totalVolumeCredits()) << " credits" << endl;
+    return move(result).str();
+}
+
+// 拆分阶段(154)
+// 提炼函数(106)
+string statement03(const invoice_t& invoce, const plays_t& plays) {
+    // 搬移函数(198)
+    auto playFor = [&plays](const performance_t& aPerformance) -> const play_t& {
+        return plays.at(aPerformance.playID);
+    };
+
     auto amountFor = [&](const enriched_performance_t& aPerformance) {
         int result = 0;
 
@@ -52,61 +105,10 @@ string renderPlainText(const statement_data_t& data, const plays_t& plays) {
         return result;
     };
 
-    auto volumeCreditsFor = [&](const enriched_performance_t& aPerformance) {
-        int result = 0;
-        result += max(aPerformance.audience - 30, 0);
-        if (COMEDY_TYPE == aPerformance.play.type) {
-            result += floor(aPerformance.audience / 5);
-        }
-        return result;
-    };
-
-    auto usd = [](int aNumber) {
-        string number_str = to_string(float(aNumber)/100);
-        return "$" + number_str.substr(0, number_str.find(".") + 3);
-    };
-
-    auto totalVolumeCredits = [&]() {
-        int result = 0;
-        for (auto& perf : data.performances) {
-            result += volumeCreditsFor(perf);
-        }
-        return result;
-    };
-
-    auto totalAmount = [&]() {
-        int result = 0;
-        for (auto& perf : data.performances) {
-            result += amountFor(perf);
-        }
-        return result;
-    };
-
-    ostringstream result;
-    result << "Statement for " << data.customer << endl;
-
-    for (auto& perf : data.performances) {
-        result << "  " << perf.play.name + ": "
-            << usd(amountFor(perf)) << " ("
-            << to_string(perf.audience) << " seats)" << endl;
-    }
-
-    result << "Amount owed is " << usd(totalAmount()) << endl;
-    result << "You earned " << to_string(totalVolumeCredits()) << " credits" << endl;
-    return move(result).str();
-}
-
-// 拆分阶段(154)
-// 提炼函数(106)
-string statement03(const invoice_t& invoce, const plays_t& plays) {
-    // 搬移函数(198)
-    auto playFor = [&plays](const performance_t& aPerformance) -> const play_t& {
-        return plays.at(aPerformance.playID);
-    };
-
     enriched_performances_t enriched_performances;
     for_each(invoce.performances.begin(), invoce.performances.end(), [&](auto& aPerformance) {
         enriched_performances.emplace_back(aPerformance, playFor(aPerformance));
+        enriched_performances.back().amount = amountFor(enriched_performances.back());
     });
 
     statement_data_t statementData{
