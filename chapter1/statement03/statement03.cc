22f9e3f091f2188bfcee7b02d65a227e24505177
wpaladins <wpaladins@163.com>
2023-04-18 07:50:18 +0800

    chapter1 - statement03 - 9

--- chapter1/statement03/statement03.cc
+++ chapter1/statement03/statement03.cc
@@ -9,26 +9,6 @@
 
 using namespace std;
 
-// 复制 performance_t 中的信息, 而其他信息仅引用
-struct enriched_performance_t : public performance_t {
-    const play_t& play;
-    int amount;
-    int volumeCredits;
-    enriched_performance_t(const performance_t& parent,
-                           const play_t& play)
-                           : performance_t(parent),
-                             play(play) {}
-};
-
-using enriched_performances_t = vector<enriched_performance_t>;
-
-struct statement_data_t {
-    const string& customer; // 使用 const &
-    const enriched_performances_t& performances;
-    int totalAmount;
-    int totalVolumeCredits;
-};
-
 // 保持数据不可变(不修改传给函数的参数)
 string renderPlainText(const statement_data_t& data, const plays_t& plays) {
     auto usd = [](int aNumber) {
@@ -52,7 +32,7 @@ string renderPlainText(const statement_data_t& data, const plays_t& plays) {
 
 // 拆分阶段(154)
 // 提炼函数(106)
-string statement03(const invoice_t& invoce, const plays_t& plays) {
+statement_data_t createStatementData(const invoice_t& invoce, const plays_t& plays) {
     // 搬移函数(198)
     auto playFor = [&plays](const performance_t& aPerformance) -> const play_t& {
         return plays.at(aPerformance.playID);
@@ -116,10 +96,14 @@ string statement03(const invoice_t& invoce, const plays_t& plays) {
 
     statement_data_t statementData{
         invoce.customer,
-        enriched_performances
+        move(enriched_performances) // pay attention
     };
     statementData.totalAmount = totalAmount(statementData);
     statementData.totalVolumeCredits = totalVolumeCredits(statementData);
 
-    return renderPlainText(statementData, plays);
+    return statementData;
+}
+
+string statement03(const invoice_t& invoice, const plays_t& plays) {
+    return renderPlainText(createStatementData(invoice, plays), plays);
 }
--- chapter1/statement03/statement03_test.cc
+++ chapter1/statement03/statement03_test.cc
@@ -1,6 +1,9 @@
 #include "statement03.h"
 
 #include <gtest/gtest.h>
+#include <vector>
+
+using namespace std;
 
 // 剧目
 plays_t plays
@@ -46,6 +49,23 @@ invoices_t invoices
         }
     }
 };
+
+TEST(StatementTestSuite, CreateStatementDataTestCase03) {
+    statement_data_t statementData = createStatementData(invoices[0], plays);
+
+    ASSERT_EQ(statementData.customer, invoices[0].customer);
+    EXPECT_EQ(statementData.totalAmount, 173000);
+    EXPECT_EQ(statementData.totalVolumeCredits, 47);
+
+    ASSERT_EQ(statementData.performances.size(), 3);
+    vector<int> expectedAmounts{65000, 58000, 50000};
+    vector<int> expectedAudience{55, 35, 40};
+    for (int i = 0; i < 3; i++) {
+        ASSERT_EQ(statementData.performances[i].playID, invoices[0].performances[i].playID);
+        EXPECT_EQ(statementData.performances[i].amount, expectedAmounts[i]);
+        EXPECT_EQ(statementData.performances[i].audience, expectedAudience[i]);
+    }
+}
 
 TEST(StatementTestSuite, StatementTestCase03) {
     EXPECT_STREQ(statement03(invoices[0], plays).c_str(), "\
