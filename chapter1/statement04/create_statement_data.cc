35ebc88e54aa1d5aeaf81ede946af2f577eb641a
wpaladins <wpaladins@163.com>
2023-04-18 23:16:58 +0800

    chapter1 - statement04 - 5

--- chapter1/statement04/create_statement_data.cc
+++ chapter1/statement04/create_statement_data.cc
@@ -2,6 +2,7 @@
 
 #include <algorithm>
 #include <cmath>
+#include <memory>
 #include <numeric>
 
 using namespace std;
@@ -52,6 +53,10 @@ private:
     const performance_t& performance;
 };
 
+unique_ptr<PerformanceCalculator> createPerformanceCalculator(const performance_t& aPerformance, const play_t& aPlay) {
+    return make_unique<PerformanceCalculator>(aPerformance, aPlay);
+}
+
 statement_data_t createStatementData(const invoice_t& invoce, const plays_t& plays) {
     auto playFor = [&plays](const performance_t& aPerformance) -> const play_t& {
         return plays.at(aPerformance.playID);
@@ -71,10 +76,10 @@ statement_data_t createStatementData(const invoice_t& invoce, const plays_t& pla
 
     enriched_performances_t enriched_performances;
     for_each(invoce.performances.begin(), invoce.performances.end(), [&](auto& aPerformance) {
-        PerformanceCalculator calculator{aPerformance, playFor(aPerformance)};
-        enriched_performances.emplace_back(aPerformance, calculator.play);
-        enriched_performances.back().amount = calculator.amount();
-        enriched_performances.back().volumeCredits = calculator.volumeCredits();
+        auto calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
+        enriched_performances.emplace_back(aPerformance, calculator->play);
+        enriched_performances.back().amount = calculator->amount();
+        enriched_performances.back().volumeCredits = calculator->volumeCredits();
     });
 
     statement_data_t result{
